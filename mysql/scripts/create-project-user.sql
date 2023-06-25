2f0d0a5bfb7cd8495fb6de03b53f76f80507026f
Breno1112 <contato.brenofachini@hotmail.com>
2023-06-25 20:40:30 -0300

    creating user and database

---
+++ mysql/scripts/create-project-user.sql
@@ -0,0 +1,3 @@
+CREATE USER IF NOT EXISTS 'userapi@%' IDENTIFIED BY 'userapipassword';
+GO
+GRANT ALL ON `project`.* TO 'userapi@%'
---
+++ stop.sh
@@ -0,0 +1,2 @@
+docker-compose -f ./deploy/compose/docker-compose.yml stop;
+docker-compose -f ./deploy/compose/docker-compose.yml rm -f
