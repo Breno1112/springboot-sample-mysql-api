609610cd4a4f80f0be7672b1908803826993df78
Breno1112 <contato.brenofachini@hotmail.com>
2023-06-29 07:10:18 -0300

    added delete endpoint

--- app/src/main/java/com/breno/api/sample/mysql/controllers/UserController.java
+++ app/src/main/java/com/breno/api/sample/mysql/controllers/UserController.java
@@ -2,13 +2,17 @@ package com.breno.api.sample.mysql.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.breno.api.sample.mysql.domain.dtos.request.CreateUserRequestDTO;
 import com.breno.api.sample.mysql.domain.dtos.response.CreateUserResponseDTO;
+import com.breno.api.sample.mysql.domain.dtos.response.DeleteResponseDTO;
+import com.breno.api.sample.mysql.domain.dtos.response.ResponseDataDTO;
 import com.breno.api.sample.mysql.domain.dtos.response.UserDTO;
 import com.breno.api.sample.mysql.services.UserService;
 import reactor.core.publisher.Mono;
@@ -27,5 +31,15 @@ public class UserController {
     @PostMapping("/user")
     public Mono<CreateUserResponseDTO> createUser(@RequestBody CreateUserRequestDTO body) {
         return this.userService.createUser(body);
+    }
+
+    @GetMapping("/users/{id}")
+    public Mono<ResponseDataDTO<UserDTO>> getUser(@PathVariable("id") String id) {
+        return this.userService.getUser(id);
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Mono<ResponseDataDTO<DeleteResponseDTO>> deleteUser(@PathVariable("id") String id) {
+        return this.userService.deleteUser(id);
     }
 }
--- app/src/main/java/com/breno/api/sample/mysql/services/UserService.java
+++ app/src/main/java/com/breno/api/sample/mysql/services/UserService.java
@@ -2,10 +2,13 @@ package com.breno.api.sample.mysql.services;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.stereotype.Service;
 
 import com.breno.api.sample.mysql.domain.dtos.request.CreateUserRequestDTO;
 import com.breno.api.sample.mysql.domain.dtos.response.CreateUserResponseDTO;
+import com.breno.api.sample.mysql.domain.dtos.response.DeleteResponseDTO;
+import com.breno.api.sample.mysql.domain.dtos.response.ResponseDataDTO;
 import com.breno.api.sample.mysql.domain.dtos.response.UserDTO;
 import com.breno.api.sample.mysql.domain.repositories.UserRepository;
 import com.breno.api.sample.mysql.mappers.UserMapper;
@@ -38,5 +41,46 @@ public class UserService {
             toReturn.setSuccess(false);
             toReturn.setError("Um erro ocorreu ao tentar criar o usuário");
         });
+    }
+
+    public Mono<ResponseDataDTO<UserDTO>> getUser(String id) {
+        return Mono.just(Integer.parseInt(id))
+            .flatMap(value -> {
+                return this.userRepository.findById(value);
+            })
+            .flatMap(record -> {
+                return Mono.just(this.userMapper.fromUserEntityToUserDTO(record));
+            })
+            .flatMap(response -> {
+                ResponseDataDTO<UserDTO> responseDataDTO = new ResponseDataDTO<UserDTO>();
+                responseDataDTO.setData(response);
+                return Mono.just(responseDataDTO);
+            })
+            .onErrorResume(error -> {
+                ResponseDataDTO<UserDTO> responseDataDTO = new ResponseDataDTO<UserDTO>();
+                responseDataDTO.setError("Erro ao encontrar usuário");
+                return Mono.just(responseDataDTO);
+            })
+            .switchIfEmpty(Mono.just(new ResponseDataDTO<UserDTO>()));
+    }
+
+    public Mono<ResponseDataDTO<DeleteResponseDTO>> deleteUser(String id) {
+        return Mono.just(Integer.parseInt(id))
+            .flatMap(value -> {
+                return this.userRepository.deleteById(value);
+            })
+            .flatMap(response -> {
+                ResponseDataDTO<DeleteResponseDTO> responseDataDTO = new ResponseDataDTO<DeleteResponseDTO>();
+                DeleteResponseDTO deleteResponseDTO = new DeleteResponseDTO();
+                deleteResponseDTO.setDeleted(true);
+                responseDataDTO.setData(deleteResponseDTO);
+                return Mono.just(responseDataDTO);
+            })
+            .onErrorResume(error -> {
+                ResponseDataDTO<DeleteResponseDTO> responseDataDTO = new ResponseDataDTO<DeleteResponseDTO>();
+                responseDataDTO.setError("Erro ao remover usuário");
+                return Mono.just(responseDataDTO);
+            })
+            .switchIfEmpty(Mono.just(new ResponseDataDTO<DeleteResponseDTO>(new DeleteResponseDTO(true), null)));
     }
 }
